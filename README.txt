4f72315570e23c1311901362bb7b039d5aa97ef8
medhapandey63 <medhabioinfo@gmail.com>
2023-04-03 19:57:34 +0530

    my first project

--- README.txt
+++ README.txt
@@ -1,9 +1,32 @@
-This program can be used to calculate sequence based features from protein sequences and also be used to derive properties based on mutations.
+This is a python based program which can be used to calculate sequence based features from protein sequences and also be used to derive properties based on mutations.
 
-Dependencies:
-1. Python 3
-2. Libraries:
-	Numpy
-	Pandas
-	Multiprocessing
-	Biopython
+You can download the librarirs using "requirements.txt" file with following command:
+   pip install -r requirements.txt
+   
+This program uses several other methods for obtaining features such as:
+	1. Psi-BLAST for Position Spcifica Scoring matrices (PSSM) (https://www.ncbi.nlm.nih.gov/books/NBK279690/)
+	2. AACon for obtaining conservation scores (https://www.compbio.dundee.ac.uk/aacon/docs/library.html)
+	3. NetsurfP for predicted secondary structures (https://services.healthtech.dtu.dk/services/NetSurfP-2.0/)
+   	For example file, I have provided the relevant features to be calculated in the data directory. User can see the detailed description of the above mentioned programs in the provide links.
+   	
+This program calculates following properties:
+	1. Sequence based:
+		Molecular weight, secondary structures, physicochemical properties, compositions of amino acids, encoded feature
+	2. Properties for the mutations:
+		Physicochemical properties
+		PSSMs
+		Conservation scores
+		
+User requires:
+	1. An input file containing the space separated information on fasta files name and comma separated mutations (input_file.txt)
+	2. Fasta file for the respective protein in "example" directory (protein.fasta)
+	3. Required feature files for the protein of interest in "data" directory (protein.features, protein.pssm, protein_netsurfp.csv)
+	
+		
+HOw to use the code:
+1. Extract the zipped file and keep the directories in the exact paths
+2. To run the program, use the following command from the terminal:
+	python sample_code.py --input_file input_file.txt
+
+
+
--- sample_code.py
+++ sample_code.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 30 14:44:41 2023
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: HP
-"""
+
+
 import os, re
 import numpy as np
 from Bio import SeqIO
@@ -11,16 +10,39 @@ from Bio.SeqUtils import ProtParam
 from collections import Counter
 import pandas as pd
 import multiprocessing as mp
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file",help = "Enter input file in space saperated format where first column represents fasta file name and second column contains comma separated mutation list")
+    args = parser.parse_args()
+
 # Open the FASTA file and read in the sequence
-#filename = input("Enter the name of the FASTA file: ")
-filename = "P04637.txt"
-# mutation = input("List the mutations separated by comma: ")
-mutation = 'P8V'
-list_mut  = mutation.split()
+#filename = input("Enter the name of the input file: ")
+filename = args.input_file
+# filename = /home/fathima/Sample_code/input_file.txt"
+inputfile = open(filename, "r").readlines()
+filename1 = [i.strip().split(" ") for i in inputfile]
+
+
+for lines in filename1:
+    fastafile = lines[0]
+    list_mut = lines[1].split(',')
+    
+motif_list = ['nM','Mc', 'n_M', 'M_c', 'n__M','M__c', 'tri']
+netsurfp = pd.read_csv("./data/"+fastafile.split('.')[0]+"_netsurfp.csv")
+physicochem_properties_actual = pd.read_csv("./data/49_properties_numerical_Values.csv")
+aacon_header = ['mut_pos','KABAT', 'JORES', 'SCHNEIDER', 'SHENKIN', 'GERSTEIN',
+                'TAYLOR_GAPS', 'TAYLOR_NO_GAPS', 'VELIBIL', 'KARLIN', 'ARMON',
+                'THOMPSON', 'NOT_LANCET', 'MIRNY', 'WILLIAMSON', 'LANDGRAF', 
+                'SANDER', 'VALDAR', 'SMERFS']
+
+
+
 all_prop = pd.DataFrame()
 # =============================================================================
 # Sequence based properties calculated using Biopython module
-record = SeqIO.read(filename, "fasta")
+record = SeqIO.read("./example/"+fastafile, "fasta")
 sequence = str(record.seq)
 # Calculate the sequence-based features
 pp = ProtParam.ProteinAnalysis(sequence)
@@ -32,6 +54,8 @@ gravy = pp.gravy()
 instability_index = pp.instability_index()
 iso_point = pp.isoelectric_point()
 secondary_struct = pp.secondary_structure_fraction()
+list_prop_pp = [pp,mol_weight,aa_count, charge, gravy,
+                instability_index, iso_point, secondary_struct]
 
 # =============================================================================
 # 
@@ -56,10 +80,9 @@ def read_fasta(file, list_mut):
         array = fasta.split('\n')
         header, sequence = array[0].strip(), re.sub('[^ACDEFGHIKLMNPQRSTUVWY-]', '-', ''.join(array[1:]).upper())
         fasta_sequences.append([header, sequence, list_mut])
-    # for mut in list_mut:
-        # fasta_sequences.append()
     return fasta_sequences
-seq = read_fasta("P04637.txt", ['P8V'])
+seq = read_fasta("./example/"+fastafile, list_mut)
+
 ######### Function to calcualate the amino acid composition ##########
 def _AAC(fasta_list):
     try:
@@ -79,72 +102,30 @@ def _AAC(fasta_list):
                 code.append(count[aa])
             encodings.append(code)
         encodings = np.array(encodings)
-        encodings = pd.DataFrame(encodings[1:, 1:].astype(float), columns=encodings[0, 1:], index=encodings[1:, 0])
+        encodings = pd.DataFrame(encodings[1:, 1:].astype(float),
+                                 columns=encodings[0, 1:], index=encodings[1:, 0])
         return encodings
     except:
         error_msg = "There is an error with the input sequence"
         print(error_msg)
         return False
-
-info = _AAC(seq)
 # =============================================================================
-# 
+# Function for calculating mutation based properties
 # =============================================================================
 
-# Print out the results
-print("Molecular Weight: ", mol_weight)
-print("Amino Acid Count: ", aa_count)
-print("Amino Acid Percentages: ", aa_percent)
-print("Charge at pH 7.4: ", charge)
-print("Gravy: ", gravy)
-print("Instability Index: ", instability_index)
-print("Isoelectric Point: ", iso_point)
-print("Secondary Structure Fractions: ", secondary_struct)
-
-
+############  Preparation of a dataframe to obtain individual mutations in a sequence
 list_names = ['name', 'sequence', 'mutations list']
 df_input = pd.DataFrame(columns=list_names)
 for i in seq:
     df_input.loc[len(df_input)] = i 
 dataset = df_input.explode('mutations list').reset_index(drop=True)
 
-#dataset = pd.read_csv('/home/fathima/Medha/Medh_MutBLESS_revision/generalized_dataset/dataset_other_cancers.csv')
-Polar = ['N','Q', 'S', 'T', 'P']
-aromatic = ['Y', 'F', 'W']
-pos_charge = ['K', 'R', 'H']
-Neg_charge = ['D', 'E']
-Sul_containing = ['C','M']
-Aliphatic = ['G','A', 'L','I', 'V']
-motif_list = ['nM','Mc', 'n_M', 'M_c', 'n__M','M__c', 'tri']
-netsurfp = pd.read_csv('P04637_netsurfp.csv')
-#physicochem_properties_normalized = pd.read_csv('/home/fathima/Medha/Medh_MutBLESS_revision/July2021/gbm_work/49_properties_normalizedValues.csv')
-physicochem_properties_actual = pd.read_csv('49_properties_numerical_Values.csv')
-#new_463 = pd.read_csv('/home/fathima/Medha/Medh_MutBLESS_revision/July2021/gbm_work/Nithya_files/463_unique_numerical_properties.csv')
-#physicochem_properties_actual1 = pd.concat([physicochem_properties_actual, new_463[physicochem_properties_actual.columns]]).reset_index(drop =True)
-aacon_header = ['mut_pos','KABAT', 'JORES', 'SCHNEIDER', 'SHENKIN', 'GERSTEIN',
-                'TAYLOR_GAPS', 'TAYLOR_NO_GAPS', 'VELIBIL', 'KARLIN', 'ARMON',
-                'THOMPSON', 'NOT_LANCET', 'MIRNY', 'WILLIAMSON', 'LANDGRAF', 
-                'SANDER', 'VALDAR', 'SMERFS']
-
-
-# all_prop = open('/home/fathima/Medha/Medh_MutBLESS_revision/generalized_dataset/feature_file.csv')
-
 # =============================================================================
 # This set of snippet will calculate different physicochemical properties, 
 # PSSM scores, and conservation scores calculated from different methods
 # =============================================================================
-# for i in range(5):
 
 def compute_features_mutation(i, all_prop = all_prop):
-    print("HEREEEEEEE",i)
-    #filename = input("Enter the name of the FASTA file: ")
-    # mutation = input("List the mutations separated by comma: ")
-    # list_mut  = mutation.split()
-    # all_prop = pd.DataFrame()
-    # =============================================================================
-    # Sequence based properties calculated using Biopython module
-    # record = SeqIO.read(filename, "fasta")
-    # sequence = str(record.seq)
     uniprot_id = dataset['name'][i]
     site = dataset['mutations list'][i][:-1]
     sul_c = 0
@@ -154,7 +135,6 @@ def compute_features_mutation(i, all_prop = all_prop):
     pos_c = 0
     arom = 0
     pos = int(site[1:])
-    #     sequence = next(iter(uniprot_seq.loc[uniprot_seq['uniprot_id'] == uniprot_id, 'Sequence']))
     sequence = dataset['sequence'][i]
     if len(sequence) >= pos:
         dict_prop = dict()
@@ -162,11 +142,9 @@ def compute_features_mutation(i, all_prop = all_prop):
         mut = dataset['mutations list'][i][0]
         wild_mut = wild+mut
         dict_prop['pos'] = int(site[1:])
-        # dict_prop['class'] = dataset['label'][i]
         dict_prop['Uniprot ID'] = uniprot_id
-        # dict_prop['Gene Name'] = dataset['gene'][i]
         dict_prop['Site'] = site  
-        f4 = open('P04637.pssm', 'r').readlines()[2:]
+        f4 = open("./data/"+ fastafile.split('.')[0]+".pssm", 'r').readlines()[2:]
         if pos<7:
             window_13 = sequence[:pos+6]
         elif pos+6>len(sequence):
@@ -225,14 +203,18 @@ def compute_features_mutation(i, all_prop = all_prop):
         b = sum(physicochem_properties_actual[window_13[k]] for k in range(len(window_13)))/sum(c.isalpha() for c in window_13)
         j = 0
         for property1 in physicochem_properties_actual['index']:
-            dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] == property1][wild].tolist()[0]  
+            dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] 
+                                                                     == property1][wild].tolist()[0]  
             if property1 == "pK'":
-                dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] == "pK'"][wild].tolist()[0]
+                dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index']
+                                                                         == "pK'"][wild].tolist()[0]
                 dict_prop[property1] =  b[j]
             else:
-                dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] == property1][wild].tolist()[0]
+                dict_prop[property1] = physicochem_properties_actual.loc[physicochem_properties_actual['index']
+                                                                         == property1][wild].tolist()[0]
                 dict_prop[property1] =  b[j]
-                dict_prop[property1+'_diff'] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] == property1][wild_mut[-1]].tolist()[0] - physicochem_properties_actual.loc[physicochem_properties_actual['index'] == property1][wild_mut[0]].tolist()[0]
+                dict_prop[property1+'_diff'] = physicochem_properties_actual.loc[physicochem_properties_actual['index'] 
+                                                                                 == property1][wild_mut[-1]].tolist()[0] - physicochem_properties_actual.loc[physicochem_properties_actual['index'] == property1][wild_mut[0]].tolist()[0]
             j += 1
         dict_prop['neg_charge'] = neg_c
         dict_prop['pos_charge'] = pos_c
@@ -242,20 +224,19 @@ def compute_features_mutation(i, all_prop = all_prop):
         dict_prop['aliphatic'] = aliphatic
         
         try:
-            dict1 = {'A':f4[pos].strip().split()[2:][0],'R':f4    [pos].strip().split()[2:][1],
-                'N':f4[pos].strip().split()[2:][2], 'D':f4[pos    ].strip().split()[2:][3],
-                      'C':f4[pos].strip().split()[2:][4], 'Q':f4[pos    ].strip().split()[2:][5], 
-                  'E':f4[pos].strip().split()[2:][6], 'G':f4[pos    ].strip().split()[2:][7],
-                      'H':f4[pos].strip().split()[2:][8], 'I':f4[pos    ].strip().split()[2:][9], 
-                  'L':f4[pos].strip().split()[2:][10], 'K':f4[pos    ].strip().split()[2:][11],
-                      'M':f4[pos].strip().split()[2:][12], 'F':f4[pos    ].strip().split()[2:][13], 
-                  'P':f4[pos].strip().split()[2:][14], 'S':f4[pos    ].strip().split()[2:][15],
-                      'T':f4[pos].strip().split()[2:][16], 'W':f4[pos    ].strip().split()[2:][17],
-                  'Y':f4[pos].strip().split()[2:][18], 'V':f4[pos    ].strip().split()[2:][19]}
+            dict1 = {'A':f4[pos].strip().split()[2:][0],'R':f4[pos].strip().split()[2:][1],
+                'N':f4[pos].strip().split()[2:][2], 'D':f4[pos].strip().split()[2:][3],
+                      'C':f4[pos].strip().split()[2:][4], 'Q':f4[pos].strip().split()[2:][5], 
+                  'E':f4[pos].strip().split()[2:][6], 'G':f4[pos].strip().split()[2:][7],
+                      'H':f4[pos].strip().split()[2:][8], 'I':f4[pos].strip().split()[2:][9], 
+                  'L':f4[pos].strip().split()[2:][10], 'K':f4[pos].strip().split()[2:][11],
+                      'M':f4[pos].strip().split()[2:][12], 'F':f4[pos].strip().split()[2:][13], 
+                  'P':f4[pos].strip().split()[2:][14], 'S':f4[pos].strip().split()[2:][15],
+                      'T':f4[pos].strip().split()[2:][16], 'W':f4[pos].strip().split()[2:][17],
+                  'Y':f4[pos].strip().split()[2:][18], 'V':f4[pos].strip().split()[2:][19]}
             dict_prop['pssm_score1'] =  int(dict1[site[0]])   
-        #             dict_prop['pssm_score1'] =  dict1[site[0]] 
             dict_prop['pssm_score2'] = sum([int(i) for i in dict1.values()])/20
-            dict_prop['pssm_score3'] = int(dict1[wild_mut[-1]])-int(dict1[    wild_mut[0]])  
+            dict_prop['pssm_score3'] = int(dict1[wild_mut[-1]])-int(dict1[wild_mut[0]])  
             cons= f4[pos].strip('\n')[90:].split()[21:22]
             if len(cons) ==0:
                 dict_prop['conservation']= 0
@@ -265,9 +246,9 @@ def compute_features_mutation(i, all_prop = all_prop):
         except (KeyError, IndexError) as error:
                 dict_prop['pssm_score1'] =  0
                 dict_prop['pssm_score2'] = 0
-            #         dict_prop['pssm_score3'] = 0
                 dict_prop['conservation']= 0
-        f_aacon = pd.read_csv('P04637.features', sep = '\t', skiprows=1, names = aacon_header)
+        f_aacon = pd.read_csv("./data/"+ fastafile.split('.')[0] + ".features", sep = '\t', skiprows=1, 
+                              names = aacon_header)
         try:
             mm = f_aacon.iloc[pos-1]
             for item in aacon_header:
@@ -275,25 +256,37 @@ def compute_features_mutation(i, all_prop = all_prop):
         except IndexError:
             for item in aacon_header:
                 dict_prop[item] = 0
-    #     print(uniprot_id)
-        print(dict_prop)
         all_prop = all_prop.append(dict_prop, ignore_index =True)
         return all_prop
-# # all_prop['pos'] = all_prop['pos'].astype(int)
-# pool = mp.Pool(mp.cpu_count()-2, maxtasksperchild=1)
-# re1 = pool.map(calls, range(1))
-# # re = pool.map(calls, range(10))
-# df11 = pd.concat(re)
+
 df11 = pd.DataFrame()
-# for i in range(len(dataset)):
-def mp_fuc(i):
+
+def mp_func(i):
     re1 = compute_features_mutation(i)
     return re1
-    # df11 = df11.append(re1,ignore_index = True)
     
 pool = mp.Pool(mp.cpu_count()-2)
-re11 = pool.map(mp_fuc,range(len(dataset)))
+re11 = pool.map(mp_func,range(len(dataset)))
+df11 = df11.append(re11, ignore_index=True)
 
-# =============================================================================
-# Create a dataframe from the values obtained from input
-# =============================================================================
+
+#####################  Save output for mutation bsed properties ##########################
+
+df11.to_csv('./output/output_mutation_based_features.csv', index = False)
+
+#####################  Save output for mutation bsed properties ##########################
+
+info = _AAC(seq)
+dict_prop_pp = {'mol_weight': mol_weight,'aa_count': aa_count, 'charge' : charge, 
+                'gravy': gravy, 'instability_index': instability_index, 'iso_point': iso_point, 
+                'sec_struc': secondary_struct}
+for item in dict_prop_pp.keys():
+    if item == 'aa_count':
+        for aa_res in dict_prop_pp[item].keys():
+            info[aa_res] = dict_prop_pp[item][aa_res]
+    elif item == 'sec_struc':
+        info[item] = str(dict_prop_pp[item])
+    else:
+        info[item] = dict_prop_pp[item]
+info.to_csv('./output/output_protein_based_features.csv', index = False)
+
